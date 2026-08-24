b8d5bfcf96e7d7119712b4a06a606513b36af701
basil204 <nguyenmanh20403@gmail.com>
2026-08-24 23:10:00 +0700

    update code

--- api-server/db.js
+++ api-server/db.js
@@ -19,7 +19,7 @@ const pool = mysql.createPool({
 export const query = async (sql, params = []) => {
   const [rows] = await pool.query(sql, params);
   return rows;
-};
+};//aa
 
 export const execute = async (sql, params = []) => {
   const [result] = await pool.execute(sql, params);

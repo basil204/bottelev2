534aec8629687c413889639ee5363c76fc72f29b
basil204 <nguyenmanh20403@gmail.com>
2026-09-14 20:15:02 +0700

    Add domain admintest.manhit.dev to next.config.ts

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   },
   // Cho phép truy cập từ IP VPS và Domain bên ngoài mà không bị cảnh báo CORS Dev
   allowedDevOrigins: [
+    "admintest.manhit.dev",
     "localhost:8693",
     "127.0.0.1:8693",
     "163.61.110.170:8693",
